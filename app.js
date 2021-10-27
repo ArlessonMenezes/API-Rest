60121f7e7313fb2fe48bb23aeed6f270ec30550a
Arlesson Menezes <arlesson_10@hotmail.com>
2021-10-27 11:37:16 -0300

    implementação do axios e bootstrap

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors  = require('cors');
 
 const gamesController = require('./Controllers/gameController');
 const connection = require('./Database/database');
@@ -8,6 +9,8 @@ const connection = require('./Database/database');
 connection.authenticate()
     .then(() => console.log('Conexão realizada com o banco de dados!'))
     .catch(err => console.log(err));
+
+app.use(cors());
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
